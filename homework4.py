d6069baccdb1e3838d534edb4e37e842e39dac2c
AnnaIakushkina <barbakadze.anna@bk.ru>
2024-05-20 17:06:48 +0300

    homework4

---
+++ homework4.py
@@ -0,0 +1,7 @@
+immutable_var = 1, 2, 3, 4, False, 'Python'
+print(immutable_var)
+#immutable_var[3] = 500     # кортеж не поддерживает обращение по элементам
+#print(immutable_var_tuple)
+mutable_list = [1, 2, 'l', 'f', 'Git']
+mutable_list[3] = 500
+print(mutable_list)
